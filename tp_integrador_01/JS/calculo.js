b47f317db0dbf6539093d03a2eedcd3f004dd429
Adriana Orellana <38931328+agorellana@users.noreply.github.com>
2022-10-22 02:51:37 -0300

    corregi menues los enlaces, esilo de scroll y js de calculo

--- tp_integrador_01/JS/calculo.js
+++ tp_integrador_01/JS/calculo.js
@@ -1,15 +1,34 @@
+/**** variables ****/
+const nombre = document.getElementsByName('nombre');
+const apellido = document.getElementsByName('apellido');
+const email = document.getElementsByName('email');
 const precio = 200;
 const cantidad = document.getElementById('cantidad');
 const descuento = document.getElementById('descuento');
 const resumen = document.getElementById('resumen');
 const pagar = document.getElementById('disabledTextInput');
+const borrar = document.getElementById('borrar');
 
 resumen.addEventListener('click',() =>{
-    console.log(precio);
-    console.log(cantidad.value);
-    console.log(descuento.value);
-    console.log(pagar.placeholder);
- 
-    const calcular =((precio*cantidad.value)-(precio*cantidad.value)*descuento.value);
-    pagar.placeholder = calcular;
+        console.log(precio);
+        console.log(cantidad.value);
+        console.log(descuento.value);
+        console.log(pagar.placeholder);
+
+        /**** calculo de la compra y actualizamos input del monto a pagar ****/
+        
+        const compra = ((precio * cantidad.value) - (precio * cantidad.value) * descuento.value);
+        
+        if (cantidad.value > 0 && descuento.value > 0) {
+            pagar.placeholder = `Total a Pagar: $ ` + compra;
+        } else {
+            alert('Verifique su selección, los campos Cantidad y Categoría deben estar completos!!');
+        }
+    });
+
+
+/**** recarga la pagina y deja todo en blanco ****/
+
+borrar.addEventListener('click', () =>{
+    location.reload();
 });
